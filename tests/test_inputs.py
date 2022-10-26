0cff13de94ca0b4b9df467f7b2530eeba0d113dc
Lucas dos Santos <santoslucasantos796@gmail.com>
2022-10-26 13:06:03 -0300

    Version 2.0

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -1,20 +1,51 @@
-from src.Identifier import validateIdentifier
+import pytest
+from src.Identifier import Identifier
 
 class TestInputValues:
-    def test_ValueNull(self):
-        assert validateIdentifier(" ") == False
 
-    def test_OneCharacterAndLetter(self):
-        assert validateIdentifier("C") == True
+    @pytest.fixture(scope='module')
+    def setUp(self):
+        obj = Identifier()
+        return obj
 
-    def test_MaximumCharactersLimit(self):
-        assert validateIdentifier("C54674") == True
+    def test_ValueNull(self, setUp):
+        print("Testing the case ValueNull")
+        expected_result = False
+        result_obtained = setUp.validateIdentifier(" ")
 
-    def test_ExeedingMaximumCharacterLimit(self):
-        assert validateIdentifier("C865654") == False
+        assert result_obtained == expected_result
 
-    def test_OneCharacterAndNumber(self):
-        assert validateIdentifier("7") == False
+    def test_OneCharacterAndLetter(self, setUp):
+        print("Testing the case OneCharacterAndLetter")
+        expected_result = True
+        result_obtained = setUp.validateIdentifier("C") 
 
-    def test_SpecialCharactersIncluded(self):
-        assert validateIdentifier("C65*%a") == False
+        assert result_obtained == expected_result
+
+    def test_MaximumCharactersLimit(self, setUp):
+        print("Testing the case MaximumCharactersLimit")
+        expected_result = True
+        result_obtained = setUp.validateIdentifier("C54674")
+
+        assert result_obtained == expected_result
+
+    def test_ExeedingMaximumCharacterLimit(self, setUp):
+        print("Testing the case ExeedingMaximumCharacterLimit")
+        expected_result = False
+        result_obtained = setUp.validateIdentifier("C865654")
+
+        assert result_obtained == expected_result
+
+    def test_OneCharacterAndNumber(self, setUp):
+        print("Testing the case OneCharacterAndNumber")
+        expected_result = False
+        result_obtained = setUp.validateIdentifier("7")
+
+        assert result_obtained == expected_result
+
+    def test_SpecialCharactersIncluded(self, setUp):
+        print("Testing the case SpecialCharactersIncluded")
+        expected_result = False
+        result_obtained = setUp.validateIdentifier("C65*%a")
+
+        assert result_obtained == expected_result
